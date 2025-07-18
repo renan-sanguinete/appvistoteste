1a2f844ea99047a8d8307c9d620e2bc45764e105
Renan Franca <renansf82@gmail.com>
2025-07-18 17:00:37 -0300

    Ajustando a tela de Loading

--- src/components/LoadingList.tsx
+++ src/components/LoadingList.tsx
@@ -1,9 +1,10 @@
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, View, Text } from "react-native";
 
 export default function LoadingList () {
     return (
-        <View className="flex-1 justify-center items-center bg-white">
+        <View className="flex-1 justify-center items-center bg-slate-300">
             <ActivityIndicator size="large" />
+            <Text className="text-xl font-semibold text-gray-600 mt-4">Carregando fotos</Text>      
         </View>
     );
 }
